db29db62f0e0075fe13488464541ea9534c5ec44
Sklyarenko Aleksandr <work.sklyar@yandex.ru>
2025-12-12 19:57:30 +0300

    .

--- 9-weather-api/city/city.routes.js
+++ 9-weather-api/city/city.routes.js
@@ -1,20 +1,23 @@
 import { Router } from 'express'
 import { formatWeatherReport } from '../helper/formatReport.js'
 import { getIcon, getWeather } from '../services/api.service.js'
-import { printWeather } from '../services/log.service.js'
 
 const router = Router()
 
 router.route('/weather').get(async (req, res) => {
-	const { city } = req.data
+	const { city } = req.body
 	if (!city) {
 		return res.status(400).json({ message: 'Укажите город' })
 	}
-	const data = await getWeather(city)
-	const weather = {
-		description: formatWeatherReport(data)
+	try {
+		const data = await getWeather(city)
+		const weather = {
+			description: formatWeatherReport(data)
+		}
+		res.json(weather)
+	} catch (e) {
+		console.error(e)
 	}
-	res.json(weather)
 })
 
 export default router
